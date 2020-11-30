d5a2b82f61fca7d91864fa8cdd4480a3651cdb7e
Alan Luo <i@iltc.io>
2020-11-30 12:53:02 -0500

    Add Auth

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
 Route::get('/burndown', function () {
     return view('burndown');
-});
+})->middleware('auth');
